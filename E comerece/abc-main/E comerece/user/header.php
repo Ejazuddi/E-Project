19f1bf6a716214b749f503811c206d4aa6eaf7e1
Ejazuddi <82398649+Ejazuddi@users.noreply.github.com>
2021-10-13 20:42:47 +0500

    Add files via upload

---
+++ E comerece/abc-main/E comerece/user/header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/e20adfb025.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+</head>
+<body>
+    
+<nav class="navbar navbar-light bg-light">
+  <div class="container-fluid font-monospace">
+    <a class="navbar-brand pb-2"><img src="01.png " alt="" width="100"></a>
+
+    <div class="d-flex">
+
+    <a href=""class="text-warning text-decoration-none pe-2"><i class="fas fa-home"></i>  Home</a>
+    <a href=""class="text-warning text-decoration-none pe-2"><i class="fas fa-shopping-cart"></i> Cart(0) |</a>
+
+    <span class="text-warning pe-2" >
+       
+    <i class="fas fa-users"></i>Hello, |
+    <a href=""class="text-warning text-decoration-none pe-2"><i class="fas fa-sign-in-alt"></i>Login</a> |
+    <a href="../admin/mystore.php"class="text-warning text-decoration-none pe-2" >admin</a>
+
+    </span>
+
+    <!-- <form class="d-flex">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button> 
+    </form> -->
+  
+</nav>
+</div>
+<div class="bg-danger font-monospace">
+  <ul class="list-unstyled d-flex justify-content-center">
+    <li><a href=""class="text-decoration-none text-white fw-bold fs-4 px-5">LAPTOPS</a></li>
+    <li><a href=""class="text-decoration-none text-white  fw-bold fs-4 px-5">Mobile</a></li>
+    <li><a href=""class="text-decoration-none text-white fw-bold fs-4 px-5">BAG</a></li>
+
+  </ul>
+</div>
+</body>
+</html>
